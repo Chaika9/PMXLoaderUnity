9b0741fc2186b2f21ed2e7d8742e6e0e74260739
Chaika9 <chaika9.perso@gmail.com>
2023-03-07 01:13:13 +0100

    Add MmdGameObject and BasicDemoController for Test

---
+++ Assets/Scripts/LibMMD/Unity3D/BasicDemoController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace LibMMD.Unity3D {
+    public class BasicDemoController : MonoBehaviour {
+        [SerializeField] private string modelPath;
+
+        private void Start() {
+            if (string.IsNullOrEmpty(modelPath)) {
+                Debug.LogError("Please fill your model path");
+                return;
+            }
+            
+            GameObject mmdGameObject = MmdGameObject.CreateGameObject("MmdGameObject");
+            var mmdGameObjectComponent = mmdGameObject.GetComponent<MmdGameObject>();
+            
+            mmdGameObjectComponent.LoadModel(modelPath);
+        }
+    }
+}
---
+++ Assets/Scripts/LibMMD/Unity3D/MmdGameObject.cs
@@ -0,0 +1,29 @@
+﻿using LibMMD.Model;
+using LibMMD.Reader;
+using UnityEngine;
+
+namespace LibMMD.Unity3D {
+    [RequireComponent(typeof(MeshFilter), typeof(SkinnedMeshRenderer))]
+    public class MmdGameObject : MonoBehaviour {
+        private MmdModel _model;
+        
+        public static GameObject CreateGameObject(string name) {
+            var go = new GameObject(name);
+            go.AddComponent<MmdGameObject>();
+            var skinnedMeshRenderer = go.GetComponent<SkinnedMeshRenderer>();
+            skinnedMeshRenderer.quality = SkinQuality.Bone4;
+            return go;
+        }
+        
+        public void LoadModel(string path) {
+            Debug.LogFormat("Start load model {0}", path);
+            try {
+                _model = ModelReader.LoadModel(path);
+            } catch (System.Exception e) {
+                Debug.LogErrorFormat("Load model {0} failed: {1}", path, e);
+            }
+            
+            Debug.LogFormat("Load model finished {0}", path);
+        }
+    }
+}
